01155b961823ca2dd9e269762e6c543bfe778bc6
julianhernao.marin.contractor.gdevtools.com <julianhernao.marin.contractor@gdevtools.com>
2025-03-31 17:47:19 -0500

    Add initial code

---
+++ src/main/java/com/example/spring_boot_ehcache_cache/SpringBootEhcacheCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_ehcache_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEhcacheCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEhcacheCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_ehcache_cache/SpringBootEhcacheCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_ehcache_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEhcacheCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
